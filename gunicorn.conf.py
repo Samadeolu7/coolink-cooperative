1bade7a4576763498f66e4806139fce125af144a
Samuel <samadeolu7@gmail.com>
2023-09-21 00:03:00 +0100

    gunicorn configuration

---
+++ gunicorn.conf.py
@@ -0,0 +1,9 @@
+# gunicorn.conf.py
+
+import multiprocessing
+
+bind = "127.0.0.1:8080"
+workers = multiprocessing.cpu_count() * 2 + 1
+errorlog = "error.log"
+accesslog = "access.log"
+loglevel = "info"
